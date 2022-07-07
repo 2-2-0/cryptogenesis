2320e3511ddf82c81ca88db94ea3175c932f39c0
220 <2-2-0@users.noreply.github.com>
2022-07-07 03:04:45 -0500

    New version!! includes a more solid POE/W system around O generation

--- bio-chain-collector/bio-chain-collector.ino
+++ bio-chain-collector/bio-chain-collector.ino
@@ -1,4 +1,4 @@
-// bio-chain-collector.ino - a POE/W system for organic data harvesting
+// bio-chain-collector.ino - a POE/W client for organic resources harvesting
 // code by 220 for MP, IV & JG | 2022
 
 #include <Crypto.h>
@@ -12,22 +12,72 @@ SHA256 sha;
 uint8_t hash [HASH_SIZE];
 
 String node_id = "0b1b7a78-2a13-4029-ac6b-d6bdbad6e96";
+String input = "";
+
+String challenge_response;
+
+boolean waiting = false;
 
 void setup() {
-  Serial.setTimeout (20);
+  Serial.setTimeout (35);
   Serial.begin (115200);
   Serial.flush ();
-}
 
-void loop() {
-  spark ();
-  delay (20);
+  delay (250);
+  requestChallenge ();
 }
-
-void spark () {
+void requestChallenge () {
   // requests new operation and identifies by using node_id
   Serial.print ("NOP:"+node_id);
   Serial.print ('\n');
+
+  waiting = true;
+}
+
+
+void loop() {
+  if (Serial.available ()>0) {
+    input = Serial.readStringUntil ('\n');
+  }
+  if (input!="") {
+    if (input.indexOf ("CH:")!=-1) {
+      // got challenge
+      input = input.substring (3);
+      solveChallenge (input);
+      waiting = true;
+    } else
+    if (input.indexOf ("RQ:0")!=-1) {
+      // got challenge response query
+      Serial.print ("CHR:"+challenge_response);
+      Serial.print ('\n');
+      challenge_response = "";
+      waiting = false;
+    } else
+    if (input.indexOf ("ACK:")!=-1 || input.indexOf ("DROP:")!=-1) {
+      requestChallenge ();
+    }
+    input = "";
+  }
+}
+void solveChallenge (String challenge) {
+  // hashes challenge using SHA256 - POE/W
+  sha.reset ();
+  sha.update (challenge.c_str (), challenge.length ());
+  sha.finalize (hash, HASH_SIZE);
+
+
+  // sends answer back to blockchain
+  String pair;
+  challenge_response = "";
+  for (int i=0; i<sizeof (hash); i++) {
+    pair = String (hash [i], HEX);
+    if (pair.length ()<2) pair = "0"+pair;
+    challenge_response+= pair;
+  }
+}
+
+void spark () {
+  
   
   // receives challenge string (a uuid)
   String challenge_string = Serial.readStringUntil ('\n');
